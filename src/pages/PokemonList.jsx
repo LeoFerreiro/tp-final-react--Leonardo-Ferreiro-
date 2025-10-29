70259808ae97b5cab723cd30176c0612faf60fa6
LeoFerreiro <leoferreiro1@gmail.com>
2025-10-29 19:56:06 -0300

    sera

--- src/pages/PokemonList.jsx
+++ src/pages/PokemonList.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./PokemonList.css";
+import "../styles/list.css";
 
 function PokemonList() {
   const [pokemons, setPokemons] = useState([]);
