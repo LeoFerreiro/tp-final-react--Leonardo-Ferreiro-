c413447e6f0773522659ec62e787f859ac2b161f
LeoFerreiro <leoferreiro1@gmail.com>
2025-10-29 20:25:48 -0300

    adkldk

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,15 @@ import "../styles/navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
+    <div className="navbar-container">
       <h1 className="logo">PokéApp</h1>
       <ul className="nav-links">
         <li><NavLink to="/" className="link">Inicio</NavLink></li>
         <li><NavLink to="/pokemons" className="link">Pokémons</NavLink></li>
       </ul>
+      </div>
     </nav>
+    
   );
 }
 
