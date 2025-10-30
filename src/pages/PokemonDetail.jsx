db11c1a18e81cce26be6cc3a173c525dfeabb814
LeoFerreiro <leoferreiro1@gmail.com>
2025-10-30 19:14:49 -0300

    dioss

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { fetchPokemonByName, addFavorite, removeFavorite } from "../redux/pokemonSlice";
+import { fetchPokemonByName, addFavorite, removeFavorite } from "../store/pokemonSlice.js";
 import "../styles/detail.css";
 
 export default function PokemonDetail() {
